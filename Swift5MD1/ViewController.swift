cc7fa148b46412f6a99b91214efbdade6a9e50cd
takumi.yamaoka <tk-y@docomo.ne.jp>
2020-10-01 07:48:02 +0900

    Initial Commit

---
+++ Swift5MD1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5MD1
+//
+//  Created by 山岡巧 on 2020/10/01.
+//  Copyright © 2020 takumi.yamaoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
