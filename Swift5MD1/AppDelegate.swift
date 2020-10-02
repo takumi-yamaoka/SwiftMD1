ecd74a54e38ed3d2f65579660713fd1100adfbcf
takumi.yamaoka <tk-y@docomo.ne.jp>
2020-10-02 15:08:09 +0900

    マルチデバイスでの表示を確認してみた。

--- Swift5MD1/AppDelegate.swift
+++ Swift5MD1/AppDelegate.swift
@@ -11,12 +11,56 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        let storyboard:UIStoryboard = self.grabStoryboard()
+               
+        if let window = window{
+                  window.rootViewController = storyboard.instantiateInitialViewController() as UIViewController?
+               }
+          self.window?.makeKeyAndVisible()
+        
         // Override point for customization after application launch.
         return true
     }
+    
+     func grabStoryboard() -> UIStoryboard{
+             
+             var storyboard = UIStoryboard()
+             let height = UIScreen.main.bounds.size.height
+             if height == 667 {
+                 storyboard = UIStoryboard(name: "Main", bundle: nil)
+                 //iPhone8
+             }else if height == 736 {
+                 storyboard = UIStoryboard(name: "iPhone8plus", bundle: nil)
+                 //iPhone8Plus
+             }else if height == 812{
+                 storyboard = UIStoryboard(name: "iPhoneXS", bundle: nil)
+             }else if height == 896{
+                 storyboard = UIStoryboard(name: "iPhone11", bundle: nil)
+             }else if height == 1112{
+                 
+                 storyboard = UIStoryboard(name: "iPad", bundle: nil)
+             }else{
+                 
+                 switch UIDevice.current.model {
+                 case "iPnone" :
+                 storyboard = UIStoryboard(name: "se", bundle: nil)
+                     break
+                 case "iPad" :
+                 storyboard = UIStoryboard(name: "iPad", bundle: nil)
+                 print("iPad")
+                     break
+                 default:
+                     break
+                 }
+             }
+             return storyboard
+     }
 
     // MARK: UISceneSession Lifecycle
 
